20c82464d52d49d5c0b1ff9379bc4032f22b52c6
Matthew Schug <mschug@nicsroyals.com>
2017-02-02 12:20:03 -0800

    Update.

---
+++ et-day6.py
@@ -0,0 +1,10 @@
+# ET
+
+a = [1,2,3]
+
+for i in a:
+    print(i)
+
+for i in range(10):
+    print(i)
+
---
+++ quiz-day7.py
@@ -0,0 +1,124 @@
+# Name: _________________________
+# 2-2-17
+# Python Quiz 1
+# Intro to CompSci
+#
+# (0.) The most important skill for a computer scientist is:
+
+
+#
+# (1.) Consider the following code. How can we make these values into a list?
+# Answer by defining a list below called mylist.
+a = 5
+b = 7
+c = 3
+d = 2
+### Your code starts here. ###
+
+### Your code ends here.   ###
+#
+# (2.) Define a function that takes two variables (a and b) as input and
+# multiplies them together. Call your function mymultiply.
+### Your code starts here. ###
+
+
+
+
+### Your code ends here.   ###
+#
+# (3.) Write the python code to ask the user for their name, and store
+# what they type in a variable called 'name'.
+### Your code starts here. ###
+
+
+
+
+### Your code ends here.   ###
+#
+# (4.) Write an if-statement that tells the user their name is too long if
+# it is longer than 5 characters. (HINT: use len() to get the length of
+# their name)
+### Your code starts here. ###
+
+
+
+
+### Your code ends here.   ###
+#
+# (5.) Write a for-loop that prints out each element of mylist (from
+# question 1).
+### Your code starts here. ###
+
+
+
+
+### Your code ends here.   ###
+#
+# (6.) Correct the following code that should add up the numbers in mylist.
+#
+mysum = 0
+for i in mylist:
+    mysum = mysum+1
+
+
+
+
+
+
+# (7.) True or False.
+#
+# _____ a.) mylist[3]==3               
+# _____ b.) (3<4) and len(mylist) != 4 
+# _____ c.) (2**4)*4 >= 32             
+# _____ d.) 3 < 4                      
+# _____ e.) (4 < 3) or (4//2 >= 3)    
+
+# (8.) Match.
+#
+# ___ a.) Problem solving            0.) A step-by-step solution to a problem
+# ___ b.) Algorithms                 1.) Read Evaluate Print Loop
+# ___ c.) REPL                       2.) converts a program to machine code
+# ___ d.) interpreter                3.) The most import skill for a C.S.
+# ___ e.) compiler                   4.) Python
+# ___ f.) >>>                        5.) The Python prompt
+# ___ g.) source code                6.) statements in a computer language
+# ___ h.) high-level language        7.) executes a high-level program
+# ___ i.) low-level language         8.) C
+# ___ j.) the language we are using  9.) Java/Python/LISP/FORTRAN/COBOL
+#
+# (9.) Name that conditional operator.
+#
+# _____ a.) Less than
+# _____ b.) Greater than
+# _____ c.) Equal to
+# _____ d.) Greater than or equal to
+# _____ e.) Less than or equal to
+# _____ f.) Not equal to
+#
+# (10.)
+#
+# Label each truth table with the appropriate logic operator.
+#
+# a.)
+# 
+# _________
+#  T T | T
+#  T F | T
+#  F T | T
+#  F F | F
+#
+# b.)
+#
+# _______
+#  T | F
+#  F | T
+#
+# c.)
+#
+# _________
+#  T T | T
+#  T F | T
+#  F T | T
+#  F F | F
+#
+
